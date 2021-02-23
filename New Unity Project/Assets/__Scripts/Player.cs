1d5ee542e96cf25bfedd88789f848d118abcdf47
aadhieaswar <aadhieaswar@gmail.com>
2021-02-22 19:59:50 -0500

    modified grass and ground textures

---
+++ New Unity Project/Assets/__Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // fields
+    [Header("Set in Inspector")]
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = Input.GetAxis("Horizontal");
+        float zPos = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xPos * speed * Time.deltaTime;
+        pos.z += zPos * speed * Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
