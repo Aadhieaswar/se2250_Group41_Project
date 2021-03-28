6191238af186e01ea5f24c5fde6ab42ccb6bcc0c
bdavidov8771 <bdavidov@uwo.ca>
2021-03-27 20:52:32 -0400

    Basic changes to character functionality

---
+++ New Unity Project/Assets/__Scripts/BulletMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Rigidbody projectile;
+    [SerializeField] private float speed;
+    void Start()
+    {
+        projectile = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        projectile.velocity = Vector3.forward * speed;
+    }
+}
---
+++ New Unity Project/Assets/__Scripts/PickUpItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject gun;
+    [SerializeField] GameObject hero;
+    private GameObject heldWeapon;
+    private GameObject pickedUpWeapon;
+    void Start()
+    {
+        heldWeapon = gun.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gun.transform.localPosition = Vector3.zero;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        heldWeapon.gameObject.SetActive(false);
+        pickedUpWeapon = other.gameObject;
+        pickedUpWeapon.transform.parent = gun.transform;
+        pickedUpWeapon.transform.position = gun.transform.position;
+        pickedUpWeapon.transform.rotation = Quaternion.Euler(hero.transform.rotation.x, hero.transform.rotation.y - 90f, hero.transform.rotation.z);
+    }
+}
