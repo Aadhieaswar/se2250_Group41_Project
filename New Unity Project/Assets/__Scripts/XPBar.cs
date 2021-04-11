33b3d699d9413538566faf5125b82f1418bf6cb6
aadhieaswar <aadhieaswar@gmail.com>
2021-04-11 12:31:29 -0400

    added status text to show HP and XP numbers dynamically in the UI

--- New Unity Project/Assets/__Scripts/XPBar.cs
+++ New Unity Project/Assets/__Scripts/XPBar.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class XPBar : MonoBehaviour
 {
     // fields
     public Slider slider;
+    public TextMeshProUGUI xpStatusText;
 
     public void SetMaxXp(int XP)
     {
         slider.maxValue = XP;
         slider.value = 0;
+
+        // set the status text
+        SetXpStatusText((int) slider.maxValue);
     }
 
     public void SetXp(int XP)
     {
         slider.value = XP;
+
+        // set the status text
+        SetXpStatusText((int) slider.maxValue, (int) slider.value);
+    }
+
+    public void SetXpStatusText(int maxXp, int XP = 0)
+    {
+        if (xpStatusText)
+            xpStatusText.SetText(XP + " / " + maxXp);
     }
 }
