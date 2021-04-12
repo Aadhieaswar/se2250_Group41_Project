17b06aa71af0907c6bd8b1a8fba16f776eb07ebd
aadhieaswar <aadhieaswar@gmail.com>
2021-04-11 22:52:44 -0400

    added basic scripts for FinalBoss and resolved bug on replay

---
+++ New Unity Project/Assets/__Scripts/Controllers/FinalBoss.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FinalBoss : MonoBehaviour
+{
+    public float lookRadius = 30f;
+    public Animator animator;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.transform.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+
+                animator.SetBool("IsAttacking", true);
+
+                // face the target
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
